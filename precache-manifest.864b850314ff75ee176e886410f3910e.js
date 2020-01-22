9ab06f0552c45c6df5f3a9b37ee1f066c86015b0
hongtree <lhwan0407@gmail.com>
2020-01-22 20:55:04 +0900

    Updates

---
+++ precache-manifest.864b850314ff75ee176e886410f3910e.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14070887711d7a8d9a09c48a93320dd9",
+    "url": "/React-PortFolio/index.html"
+  },
+  {
+    "revision": "d7b1beeeb21493d14704",
+    "url": "/React-PortFolio/static/css/main.2de41ca7.chunk.css"
+  },
+  {
+    "revision": "f89f0c983de3baa3dd04",
+    "url": "/React-PortFolio/static/js/2.64fc1b52.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-PortFolio/static/js/2.64fc1b52.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d7b1beeeb21493d14704",
+    "url": "/React-PortFolio/static/js/main.e67224b9.chunk.js"
+  },
+  {
+    "revision": "e79d26d1ce22b49d8ed0",
+    "url": "/React-PortFolio/static/js/runtime-main.e4f18b27.js"
+  },
+  {
+    "revision": "11e8af26618daecab1b13cab7387ce65",
+    "url": "/React-PortFolio/static/media/900px-JavaScript-logo.11e8af26.png"
+  },
+  {
+    "revision": "59c467536111a981d904a050d335c2be",
+    "url": "/React-PortFolio/static/media/Redux.59c46753.png"
+  },
+  {
+    "revision": "7ed8a808420a41cc91afa6236e7a1dd4",
+    "url": "/React-PortFolio/static/media/Tux.7ed8a808.png"
+  },
+  {
+    "revision": "96f372af44370c18c5fd7b46f795ad9b",
+    "url": "/React-PortFolio/static/media/Windows-Logo.96f372af.png"
+  },
+  {
+    "revision": "8be09ff1658bfb0d5ba68593e8698bb7",
+    "url": "/React-PortFolio/static/media/about_profile.8be09ff1.jpg"
+  },
+  {
+    "revision": "dd1d21febc0f2223b9b9f7a19201ed20",
+    "url": "/React-PortFolio/static/media/android.dd1d21fe.png"
+  },
+  {
+    "revision": "e93498ae890682ae4c6d829a879db988",
+    "url": "/React-PortFolio/static/media/bg2.e93498ae.jpg"
+  },
+  {
+    "revision": "58393424b2ab7ba7027f214660b11677",
+    "url": "/React-PortFolio/static/media/cpppp-490x490.58393424.png"
+  },
+  {
+    "revision": "faae11a6b87547339411b40ac228d4d7",
+    "url": "/React-PortFolio/static/media/css.faae11a6.png"
+  },
+  {
+    "revision": "8c54ec0727b7919b84cd52a2efc47814",
+    "url": "/React-PortFolio/static/media/github-logo.8c54ec07.png"
+  },
+  {
+    "revision": "5496a5aa339d56ba870ba96f7528b455",
+    "url": "/React-PortFolio/static/media/html-5.5496a5aa.png"
+  },
+  {
+    "revision": "3b96c497d8497caad056c672117d44e6",
+    "url": "/React-PortFolio/static/media/icons8-c-programming-500.3b96c497.png"
+  },
+  {
+    "revision": "23d4be78082407fbde75192f49f7f6b3",
+    "url": "/React-PortFolio/static/media/icons8-nodejs-500.23d4be78.png"
+  },
+  {
+    "revision": "74e943d82452f81f6a0bb0b51ac6785d",
+    "url": "/React-PortFolio/static/media/java.74e943d8.png"
+  },
+  {
+    "revision": "daeaaf51189d49c32411056c83360f41",
+    "url": "/React-PortFolio/static/media/mongodb-logo-1.daeaaf51.png"
+  },
+  {
+    "revision": "07c28adf28e0592b46094aef81fcc655",
+    "url": "/React-PortFolio/static/media/mysql.07c28adf.png"
+  },
+  {
+    "revision": "876a8325687358ce46ec26be29118535",
+    "url": "/React-PortFolio/static/media/react-1.876a8325.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c4870f8959992c40a19dadf5b73a1db1.js"
+  "/React-PortFolio/precache-manifest.864b850314ff75ee176e886410f3910e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-PortFolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
